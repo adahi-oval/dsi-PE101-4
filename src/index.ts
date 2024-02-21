63ec1708b6050d3d5214a2ca1c003bdc51466f58
alu0101242071 <alu0101242071@ull.edu.es>
2024-02-21 10:10:26 +0000

    checkBoard

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,29 @@
 export function hola(){
   console.log("Hola Mundo");
 }
+
+export function checkBoard(board: Array<Array<string>>): boolean {
+
+  let checker: boolean = true;
+  let allowedChars: Array<string> = ['-', 'N', 'B'];
+
+  for (let index = 0; index < board.length; index++) {
+    const element = board[index];
+    if (element.length !== 8) { checker = false; }
+    for(let square of element){
+      if(square in allowedChars) { continue; }
+      else {checker = false; }
+    }
+  }
+
+  return checker; 
+}
+
+export function checkAttack(board: Array<Array<string>>): boolean {
+
+  if (!checkBoard(board)){
+    throw new Error('El formato del tablero no es válido.');
+  }
+
+  return undefined;
+}
